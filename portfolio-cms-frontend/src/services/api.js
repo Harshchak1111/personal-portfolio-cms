41f0c9813fb98d1516e2170917af7ffb0b7f4acd
Harshchak1111 <harshchak44@gmail.com>
2025-07-31 21:59:42 +0530

    Your meaningful commit message here

--- portfolio-cms-frontend/src/services/api.js
+++ portfolio-cms-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: 'https://personal-portfolio-cms-server.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
